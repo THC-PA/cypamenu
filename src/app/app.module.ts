0a1af9159234d6a8df967cec80563d02366f24cf
Chad <chadk241@gmail.com>
2019-07-21 16:06:02 -0400

    added stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component'; 
 import { HttpClientModule } from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatIconModule, MatToolbarModule} from '@angular/material';
+import { MatCardModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule, MatTableModule, MatPaginatorModule, MatSortModule } 
+from '@angular/material';
+import { SortByPipe } from './pipes/sortBy.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SortByPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatInputModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatToolbarModule
   ],
+  exports: [SortByPipe],
   providers: [],
   bootstrap: [AppComponent]
 })
